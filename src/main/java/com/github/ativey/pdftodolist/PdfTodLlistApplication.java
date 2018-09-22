5daf5a46d43a3bb6119fd5046ac0910e344c009f
ativey <ativey@users.noreply.github.com>
2018-09-22 18:41:35 +0100

    Initial commit

---
+++ src/main/java/com/github/ativey/pdftodolist/PdfTodLlistApplication.java
@@ -0,0 +1,12 @@
+package com.github.ativey.pdftodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfTodLlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdfTodLlistApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/ativey/pdftodolist/PdfTodLlistApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.ativey.pdftodolist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PdfTodLlistApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
