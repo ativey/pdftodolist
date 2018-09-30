faab50a5f17c2ec766fd0e4957e3eb0a8dd48506
ativey <ativey@users.noreply.github.com>
2018-09-30 13:21:26 +0100

    Create PDFColours based on CSS4. Closes #18

---
+++ src/main/java/com/github/ativey/pdftodolist/com/github/ativey/pdftolist/pdf/PdfColor.java
@@ -0,0 +1,169 @@
+package com.github.ativey.pdftodolist.com.github.ativey.pdftolist.pdf;
+
+import com.itextpdf.kernel.colors.Color;
+import com.itextpdf.kernel.colors.DeviceRgb;
+
+public class PdfColor {
+
+    // Red colors
+    public static final Color LiGHT_SALMON = new DeviceRgb(255, 160, 122);
+    public static final Color SALMON = new DeviceRgb(250, 128, 114);
+    public static final Color DARK_SALMON = new DeviceRgb(233, 150, 122);
+    public static final Color LIGHT_CORAL = new DeviceRgb(240, 128, 128);
+    public static final Color INDIAN_RED = new DeviceRgb(205, 92, 92);
+    public static final Color CRIMSON = new DeviceRgb(220, 20, 60);
+    public static final Color FIREBRICK = new DeviceRgb(178, 34, 34);
+    public static final Color RED = new DeviceRgb(255, 0, 0);
+    public static final Color DARKRED = new DeviceRgb(139, 0, 0);
+
+    // Orange colors
+    public static final Color CORAL = new DeviceRgb(255, 127, 80);
+    public static final Color TOMATO = new DeviceRgb(255, 99, 71);
+    public static final Color ORANGE_RED = new DeviceRgb(255, 69, 0);
+    public static final Color GOLD = new DeviceRgb(255, 215, 0);
+    public static final Color ORANGE = new DeviceRgb(255, 165, 0);
+    public static final Color DARK_ORANGE = new DeviceRgb(255, 140, 0);
+
+    // Yellow colors
+    public static final Color LIGHT_YELLOW = new DeviceRgb(255, 255, 224);
+    public static final Color LEMON_CHIFFON = new DeviceRgb(255, 250, 205);
+    public static final Color LIGHT_GOLDENROD_YELLOW = new DeviceRgb(250, 250, 210);
+    public static final Color PAPAYAWHIP = new DeviceRgb(255, 239, 213);
+    public static final Color MOCCASIN = new DeviceRgb(255, 228, 181);
+    public static final Color PEACHPUFF = new DeviceRgb(255, 218, 185);
+    public static final Color PALE_GOLDENROD = new DeviceRgb(238, 232, 170);
+    public static final Color KHAKI = new DeviceRgb(240, 230, 140);
+    public static final Color DARKKHAKI = new DeviceRgb(189, 183, 107);
+    public static final Color YELLOW = new DeviceRgb(255, 255, 0);
+
+    // Green colors
+    public static final Color LAWN_GREEN = new DeviceRgb(124, 252, 0);
+    public static final Color CHARTREUSE = new DeviceRgb(127, 255, 0);
+    public static final Color LIME_GREEN = new DeviceRgb(50, 205, 50);
+    public static final Color LIME = new DeviceRgb(50, 255, 50);
+    public static final Color FOREST_GREEN = new DeviceRgb(34, 139, 34);
+    public static final Color GREEN = new DeviceRgb(0, 128, 0);
+    public static final Color DARK_GREEN = new DeviceRgb(0, 100, 0);
+    public static final Color GREEN_YELLOW = new DeviceRgb(173, 255, 47);
+    public static final Color YELLOW_GREEN = new DeviceRgb(154, 205, 50);
+    public static final Color SPRING_GREEN = new DeviceRgb(0, 255, 127);
+    public static final Color MEDIUM_SPRING_GREEN = new DeviceRgb(0, 250, 154);
+    public static final Color LIGHT_GREEN = new DeviceRgb(144, 238, 144);
+    public static final Color PALE_GREEN = new DeviceRgb(152, 251, 152);
+    public static final Color DARK_SEA_GREEN = new DeviceRgb(143, 188, 143);
+    public static final Color MEDIUM_SEA_GREEN = new DeviceRgb(60, 179, 113);
+    public static final Color SEA_GREEN = new DeviceRgb(46, 139, 87);
+    public static final Color OLIVE = new DeviceRgb(128, 128, 0);
+    public static final Color DARK_OLIVE_GREEN = new DeviceRgb(85, 107, 47);
+    public static final Color OLIVE_DRAB = new DeviceRgb(107, 142, 35);
+
+    // Cyan colors
+    public static final Color LIGHT_CYAN = new DeviceRgb(224, 255, 255);
+    public static final Color CYAN = new DeviceRgb(0, 255, 255);
+    public static final Color AQUA = new DeviceRgb(0, 255, 255);
+    public static final Color AQUAMARINE = new DeviceRgb(127, 255, 212);
+    public static final Color MEDIUM_AQUAMARINE = new DeviceRgb(102, 205, 170);
+    public static final Color PALE_TURQUOISE = new DeviceRgb(175, 238, 238);
+    public static final Color TURQUOISE = new DeviceRgb(64, 224, 208);
+    public static final Color MEDIUM_TURQUOISE = new DeviceRgb(72, 209, 204);
+    public static final Color DARK_TURQUOISE = new DeviceRgb(0, 206, 209);
+    public static final Color LIGHT_SEA_GREEN = new DeviceRgb(32, 178, 170);
+    public static final Color CADET_BLUE = new DeviceRgb(95, 158, 160);
+    public static final Color DARK_CYAN = new DeviceRgb(0, 139, 139);
+    public static final Color TEAL = new DeviceRgb(0, 128, 128);
+
+    // Blue colors
+    public static final Color POWDER_BLUE = new DeviceRgb(176, 224, 230);
+    public static final Color LIGHT_BLUE = new DeviceRgb(173, 216, 230);
+    public static final Color LIGHT_SKY_BLUE = new DeviceRgb(135, 206, 250);
+    public static final Color SKY_BLUE = new DeviceRgb(135, 206, 235);
+    public static final Color DEEP_SKY_BLUE = new DeviceRgb(0, 191, 255);
+    public static final Color LIGHT_STEEL_BLUE = new DeviceRgb(176, 196, 222);
+    public static final Color DODGER_BLUE = new DeviceRgb(30, 144, 255);
+    public static final Color CORNFLOWER_BLUE = new DeviceRgb(100, 149, 237);
+    public static final Color STEEL_BLUE = new DeviceRgb(70, 130, 180);
+    public static final Color ROYAL_BLUE = new DeviceRgb(65, 105, 225);
+    public static final Color BLUE = new DeviceRgb(0, 0, 255);
+    public static final Color MEDIUM_BLUE = new DeviceRgb(0, 0, 205);
+    public static final Color DARK_BLUE = new DeviceRgb(0, 0, 139);
+    public static final Color NAVY = new DeviceRgb(0, 0, 128);
+    public static final Color MIDNIGHT_BLUE = new DeviceRgb(25, 25, 112);
+    public static final Color MEDIUM_SLATE_BLUE = new DeviceRgb(123, 104, 238);
+    public static final Color SLATE_BLUE = new DeviceRgb(106, 90, 205);
+    public static final Color DARK_SLATE_BLUE = new DeviceRgb(72, 61, 139);
+
+    // Purple colors
+    public static final Color LAVENDER = new DeviceRgb(230, 230, 250);
+    public static final Color THISTLE = new DeviceRgb(216, 191, 216);
+    public static final Color PLUM = new DeviceRgb(221, 160, 221);
+    public static final Color VIOLET = new DeviceRgb(238, 130, 238);
+    public static final Color ORCHID = new DeviceRgb(218, 112, 214);
+    public static final Color FUCHSIA = new DeviceRgb(255, 0, 255);
+    public static final Color MAGENTA = new DeviceRgb(255, 0, 255);
+    public static final Color MEDIUM_ORCHID = new DeviceRgb(186, 85, 211);
+    public static final Color MEDIUM_PURPLE = new DeviceRgb(147, 112, 219);
+    public static final Color BLUE_VIOLET = new DeviceRgb(138, 43, 226);
+    public static final Color DARK_VIOLET = new DeviceRgb(148, 0, 211);
+    public static final Color DARK_ORCHID = new DeviceRgb(153, 50, 204);
+    public static final Color DARK_MAGENTA = new DeviceRgb(139, 0, 139);
+    public static final Color PURPLE = new DeviceRgb(128, 0, 128);
+    public static final Color INDIGO = new DeviceRgb(75, 0, 130);
+
+    // Pink colors
+    public static final Color PINK = new DeviceRgb(255, 192, 203);
+    public static final Color LIGHT_PINK = new DeviceRgb(255, 182, 193);
+    public static final Color HOT_PINK = new DeviceRgb(255, 105, 180);
+    public static final Color DEEP_PINK = new DeviceRgb(255, 20, 147);
+    public static final Color PALE_VIOLET_RED = new DeviceRgb(219, 112, 147);
+    public static final Color MEDIUM_VIOLET_RED = new DeviceRgb(199, 21, 133);
+
+    // White colors
+    public static final Color WHITE = new DeviceRgb(255, 255, 255);
+    public static final Color SNOW = new DeviceRgb(255, 250, 250);
+    public static final Color HONEYDEW = new DeviceRgb(240, 255, 240);
+    public static final Color MINTCREAM = new DeviceRgb(245, 255, 250);
+    public static final Color AZURE = new DeviceRgb(240, 255, 255);
+    public static final Color ALICE_BLUE = new DeviceRgb(240, 248, 255);
+    public static final Color GHOSTWHITE = new DeviceRgb(248, 248, 255);
+    public static final Color WHITESMOKE = new DeviceRgb(245, 245, 245);
+    public static final Color SEASHELL = new DeviceRgb(255, 245, 238);
+    public static final Color BEIGE = new DeviceRgb(245, 245, 220);
+    public static final Color OLD_LACE = new DeviceRgb(253, 245, 230);
+    public static final Color FLORAL_WHITE = new DeviceRgb(255, 250, 240);
+    public static final Color IVORY = new DeviceRgb(255, 255, 240);
+    public static final Color ANTIQUE_WHITE = new DeviceRgb(250, 235, 215);
+    public static final Color LINEN = new DeviceRgb(250, 240, 230);
+    public static final Color LAVENDER_BLUSH = new DeviceRgb(255, 240, 245);
+    public static final Color MISTY_ROSE = new DeviceRgb(255, 228, 225);
+
+    // Gray colors
+    public static final Color GAINSBORO = new DeviceRgb(220, 220, 220);
+    public static final Color LIGHT_GRAY = new DeviceRgb(211, 211, 211);
+    public static final Color SILVER = new DeviceRgb(192, 192, 192);
+    public static final Color DARK_GRAY = new DeviceRgb(169, 169, 169);
+    public static final Color GRAY = new DeviceRgb(128, 128, 128);
+    public static final Color DIM_GRAY = new DeviceRgb(105, 105, 105);
+    public static final Color LIGHT_SLATE_GRAY = new DeviceRgb(119, 136, 153);
+    public static final Color SLATE_GRAY = new DeviceRgb(112, 128, 144);
+    public static final Color DARK_SLATE_GRAY = new DeviceRgb(47, 79, 79);
+    public static final Color BLACK = new DeviceRgb(0, 0, 0);
+
+    // Brown colors
+    public static final Color CORN_SILK = new DeviceRgb(255, 248, 220);
+    public static final Color BLANCHED_ALMOND = new DeviceRgb(255, 235, 205);
+    public static final Color BISQUE = new DeviceRgb(255, 228, 196);
+    public static final Color NAVAJO_WHITE = new DeviceRgb(255, 222, 173);
+    public static final Color WHEAT = new DeviceRgb(245, 222, 179);
+    public static final Color BURLY_WOOD = new DeviceRgb(222, 184, 135);
+    public static final Color TAN = new DeviceRgb(210, 180, 140);
+    public static final Color ROSY_BROWN = new DeviceRgb(188, 143, 143);
+    public static final Color SANDY_BROWN = new DeviceRgb(244, 164, 96);
+    public static final Color GOLDENROD = new DeviceRgb(218, 165, 32);
+    public static final Color PERU = new DeviceRgb(205, 133, 63);
+    public static final Color CHOCOLATE = new DeviceRgb(210, 105, 30);
+    public static final Color SADDLE_BROWN = new DeviceRgb(139, 69, 19);
+    public static final Color SIENNA = new DeviceRgb(160, 82, 45);
+    public static final Color BROWN = new DeviceRgb(165, 42, 42);
+    public static final Color MAROON = new DeviceRgb(128, 0, 0);
+
+}
