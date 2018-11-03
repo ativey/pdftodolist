23bf3bf0b29339e0057d660861bd12f64c9c9d32
ativey <ativey@users.noreply.github.com>
2018-11-03 15:39:38 +0000

    Remove auto-startup 'test', replace with a link in the web interface

--- src/main/java/com/github/ativey/pdftodolist/PdfTodLlistApplication.java
+++ src/main/java/com/github/ativey/pdftodolist/PdfTodLlistApplication.java
@@ -1,41 +1,13 @@
 package com.github.ativey.pdftodolist;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.io.File;
 
 @SpringBootApplication
-public class PdfTodLlistApplication implements CommandLineRunner {
+public class PdfTodLlistApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PdfTodLlistApplication.class, args);
     }
-
-
-    @Autowired
-    YamlLoader yamlLoader;
-
-
-    @Autowired
-    YamlDbPersistence yamlDbPersistence;
-
-    @Autowired
-    PdfController pdfController;
-
-    @Override
-    @Transactional
-    public void run(String... args) throws Exception {
-
-        var categoryListMap = yamlLoader.load(new File("/home/work/Desktop/main_tasks.yaml"));
-        yamlDbPersistence.persist(categoryListMap);
-
-        pdfController.generatePdf(true, CompletedDisplayStrategy.END_OF_CATEGORY);
-
-    }
-
 
 }
